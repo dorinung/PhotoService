a74dd16dffec7f24362d55e646f7e9d50b07963e
Dorin Ungureanu <dorinung@live.com>
2024-10-28 17:46:55 +0200

    stateless web api

---
+++ PhotosWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhotosWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
